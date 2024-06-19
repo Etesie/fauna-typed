df1eba241bab15cd493674bc648e53bd52fc0611
Micha Mailaender <micha@mailaender.me>
2024-06-19 20:40:59 +0200

    Added new types

--- src/lib/types/NEW/types.ts
+++ src/lib/types/NEW/types.ts
@@ -1,16 +1,52 @@
-import { Client, fql, Page, type QuerySuccess, type DocumentT, DocumentReference } from 'fauna';
+import type { Ordering } from '$lib/stores/_shared/order';
+import {
+	type Document as FaunaDocument,
+	DocumentReference,
+	DateStub,
+	type QueryValueObject
+} from 'fauna';
+
+type Document = Omit<FaunaDocument, 'toObject'>;
+type Document_Create = Partial<Omit<Document, 'ts' | 'coll'>>;
+type Document_UpdateReplace = Omit<Document_Create, 'id'>;
+type DocumentT<T extends QueryValueObject> = Document & T;
+type Document_CreateT<T extends QueryValueObject> = Document_Create & T;
+type Document_UpdateReplaceT<T extends QueryValueObject> = Document_UpdateReplace & T;
 
 type User = {
 	firstName: string;
 	lastName: string;
+	birthdate: DateStub;
+	age: number;
 	account: Account;
 };
+/**
+ * \*_Create/Replace types don't have Computed fields and Documents replaced with Document and References
+ */
+type User_CreateReplace = Omit<User, 'age' | 'account'> & {
+	account: Account | DocumentReference;
+};
+type User_Update = Partial<User_CreateReplace>;
+
+/** Used to update documents in Fauna */
+type User_FaunaCreateReplace = Omit<User, 'Account' | 'age'> & {
+	Account: DocumentReference;
+};
+type User_FaunaUpdate = Partial<
+	Omit<User, 'Account' | 'age'> & {
+		Account: DocumentReference;
+	}
+>;
 
 type Account = {
 	user: () => Account;
 	provider: string;
 	providerUserId: string;
 };
+type Account_CreateReplace = Omit<Account, 'user'> & {
+	user: User | DocumentReference;
+};
+type Account_Update = Partial<Account_CreateReplace>;
 
 type Functions<T> = {
 	update: (document: T) => void;
@@ -20,14 +56,61 @@ type Functions<T> = {
 
 type FunctionsT<T> = Functions<T> & T;
 
+class Page<T extends Document> {
+	data: T[];
+	after?: string;
+
+	constructor(data: T[], after?: string) {
+		this.data = data;
+		if (after) {
+			this.after = after;
+		}
+	}
+
+	/**
+	 * Sorts the Page data based on provided orderings. The first entry in the Ordering has the highest sorting priority, with priority decreasing with each following entry.
+	 * @param orderings - A list of ordering functions, created by `asc` or `desc`.
+	 * @example
+	 * import { asc, desc } from 'fauna-typed/stores';
+	 * User.all().order(asc((u) => u.firstName), desc((u) => u.lastName)))
+	 */
+	order(...orderings: Ordering<T>[]): Page<T> {
+		this.data.sort((a: T, b: T) => {
+			for (const ordering of orderings) {
+				const result = ordering(a, b);
+				if (result !== 0) return result;
+			}
+			return 0;
+		});
+		return this;
+	}
+}
+
 // const mergedUser: FunctionsT<DocumentT<User>>;
 // mergedUser.account;
 
 // const mergedPageUser: Page<FunctionsT<DocumentT<User>>>;
 // mergedPageUser.data.forEach((doc) => doc.account);
 
-type NewUser = {
-	firstName: string;
-	lastName: string;
-	account: Account | DocumentReference;
+type Field = {
+	signature: string;
+};
+
+type Fields = {
+	[key: string]: Field;
+};
+
+export {
+	type DocumentT,
+	type Document_CreateT,
+	type Document_UpdateReplaceT,
+	type User,
+	type User_CreateReplace,
+	type User_Update,
+	type Account,
+	type Account_CreateReplace,
+	type Account_Update,
+	type FunctionsT,
+	Page,
+	type Fields
 };
