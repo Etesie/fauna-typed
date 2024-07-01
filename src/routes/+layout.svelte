48d73df4d3503d2c1b60bc39711a37cdaab3c9ff
Aditya Vaishnav <firehudson@gmail.com>
2024-07-01 13:41:18 +0530

    Add button to create type

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script>
 	import '../app.css';
+
+	const createTypes = async () => {
+		try {
+			const res = await fetch('/api/types');
+			const parsedRes = await res.json();
+
+			console.log({ parsedRes });
+		} catch (error) {
+			console.log('Error in createTypes:', error);
+		}
+	};
 </script>
 
+<button on:click={createTypes}>Create Types</button>
 <slot></slot>
 
 <style></style>
