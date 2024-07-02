da1c152ef144285c7411163a3f60db269ded4884
Aditya Vaishnav <firehudson@gmail.com>
2024-07-02 11:32:03 +0530

    Move api call for generate types in store/index.ts file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,5 @@
 <script>
 	import '../app.css';
-
-	// TODO: Move this to src\lib\stores\index.ts
-	const createTypes = async () => {
-		try {
-			const res = await fetch('/api/types');
-			const generatedTypesRes = await res.json();
-
-			console.log({ generatedTypesRes });
-		} catch (error) {
-			console.log('Error in createTypes:', error);
-		}
-	};
-
-	$effect(() => {
-		createTypes();
-	});
 </script>
 
 <slot></slot>
