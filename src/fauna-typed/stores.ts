1529df0174c264f2371cd0f40e09bd7f956b1a63
Aditya Vaishnav <firehudson@gmail.com>
2024-07-04 20:17:18 +0530

    Create store for all collections

--- src/fauna-typed/stores.ts
+++ src/fauna-typed/stores.ts
@@ -1,16 +1,25 @@
 import { createCollectionStore } from '$lib/stores/collection.svelte';
 import { createDocumentStore } from '$lib/stores/document.svelte';
 import { asc, desc } from '$lib/stores/_shared/order';
+import type { TypeMapping } from './types';
 import type { DocumentStores } from '$lib/types/types';
 
-let documentStores = {} as DocumentStores;
+const Collection = createCollectionStore();
 
-const stores = {
-	Collection: createCollectionStore(),
-	User: createDocumentStore('User', documentStores),
-	Account: createDocumentStore('Account', documentStores)
+type Stores = DocumentStores & {
+	Collection: typeof Collection;
 };
 
-Object.assign(documentStores, stores);
+let documentStores: DocumentStores = {} as DocumentStores;
+
+documentStores = Collection.all().data?.reduce((acc, curr) => {
+	return Object.assign(acc, {
+		[curr.name]: createDocumentStore(curr.name as keyof TypeMapping, documentStores)
+	});
+}, {} as DocumentStores);
+
+const stores: Stores = Object.assign(documentStores, {
+	Collection
+});
 
 export { stores, stores as s, asc, desc };
