3adea541a2bfb598c7eb3abb0d65a40f46cfe04d
Aditya Vaishnav <firehudson@gmail.com>
2024-07-18 20:18:19 +0530

    Add fauna's first API

--- src/lib/database/fauna.ts
+++ src/lib/database/fauna.ts
@@ -4,6 +4,7 @@ import { Client, fql, type QueryValueObject } from 'fauna';
 export type CreateDatabaseApi<T extends QueryValueObject> = {
 	all: () => Promise<void>;
 	where: (filter: Predicate<Document<T>>) => Promise<void>;
+	first: () => Promise<void>;
 };
 
 export const createDatabaseApi = <
@@ -48,8 +49,24 @@ export const createDatabaseApi = <
 		}
 	}
 
+	async function first() {
+		try {
+			const query = `${COLL_NAME}.all().first()`;
+
+			console.log('first:', query);
+			const response = await client.query<Functions<T, T_Replace, T_Update>>(fql([query]));
+			if (response.data) {
+				// Find the data in the store and replace it with the new data. If it doesn't exist, add it.
+				upsertObjectFromFauna(response.data);
+			}
+		} catch (error) {
+			console.error('Error fetching document from database using first:', error);
+		}
+	}
+
 	return {
 		all,
-		where
+		where,
+		first
 	};
 };
