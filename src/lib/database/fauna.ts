19cc5c351b96ddebaed73bb664278d4b8099b0e4
Aditya Vaishnav <firehudson@gmail.com>
2024-07-19 15:13:30 +0530

    Add fauna API for last

--- src/lib/database/fauna.ts
+++ src/lib/database/fauna.ts
@@ -5,6 +5,7 @@ export type CreateDatabaseApi<T extends QueryValueObject> = {
 	all: () => Promise<void>;
 	where: (filter: Predicate<Document<T>>) => Promise<void>;
 	first: () => Promise<void>;
+	last: () => Promise<void>;
 };
 
 export const createDatabaseApi = <
@@ -67,9 +68,25 @@ export const createDatabaseApi = <
 		}
 	}
 
+	async function last() {
+		try {
+			const query = `${COLL_NAME}.all().last()`;
+
+			console.log('last:', query);
+			const response = await client.query<Functions<T, T_Replace, T_Update>>(fql([query]));
+			if (response.data) {
+				// Find the data in the store and replace it with the new data. If it doesn't exist, add it.
+				upsertObjectFromFauna(response.data);
+			}
+		} catch (error) {
+			console.error('Error fetching document from database using last:', error);
+		}
+	}
+
 	return {
 		all,
 		where,
-		first
+		first,
+		last
 	};
 };
